a7b44bd61341f0d9751002644fef3460729a5d39
Liyadh11 <liyadhbunny@gmail.com>
2025-11-06 13:14:21 +0530

    restaruant detials modfication & fotter main tab

--- src/components/common/HomeIcon.tsx
+++ src/components/common/HomeIcon.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PlaceholderIcon from './PlaceholderIcon';
+import Svg, { Path } from 'react-native-svg';
 
 interface HomeIconProps {
   size?: number;
@@ -13,12 +13,17 @@ const HomeIcon: React.FC<HomeIconProps> = ({
   style,
 }) => {
   return (
-    <PlaceholderIcon
-      size={size}
-      color={color}
+    <Svg
+      width={size}
+      height={size}
+      viewBox="0 0 512 512"
       style={style}
-      label="H"
-    />
+    >
+      <Path
+        d="M453.794,170.688L283.185,10.753c-15.287-14.337-39.083-14.337-54.37,0L58.206,170.688 c-8.012,7.515-12.565,18.01-12.565,29V472.25c0,21.954,17.803,39.75,39.75,39.75h120.947V395.145h99.324V512h120.946 c21.947,0,39.751-17.796,39.751-39.75V199.688C466.359,188.698,461.805,178.203,453.794,170.688z"
+        fill={color}
+      />
+    </Svg>
   );
 };
 
